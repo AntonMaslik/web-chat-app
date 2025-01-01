ac4134fad7bdcf3af4a205c683fc83438b225fd7
Anton <maslik.fortech@gmail.com>
2025-01-01 16:54:36 +0300

    feat(chat): add new font - Roboto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Roboto", "ui-sans-serif", "system-ui"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
