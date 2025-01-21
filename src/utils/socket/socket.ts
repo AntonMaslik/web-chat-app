1f2f17c06a92bd1278e54626420b9f4a7d4109e8
AntonMaslik <maslik.fortech@gmail.com>
2025-01-22 01:37:41 +0300

    fix(socket): add host and port in socket object

--- src/utils/socket/socket.ts
+++ src/utils/socket/socket.ts
@@ -3,3 +3,13 @@
 import { io } from "socket.io-client";
 
 export const socket = io();
+
+"use client";
+
+import { io } from "socket.io-client";
+
+export const socket = io({
+  host: process.env.SOCKET_SERVER_HOST || "localhost",
+  port: (process.env.SOCKET_SERVER_PORT as unknown as number) || 3000,
+  transports: ["websocket"],
+});
