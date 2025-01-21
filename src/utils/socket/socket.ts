e0395b4c3369346e05e0b5096d3b2afa063683c6
AntonMaslik <maslik.fortech@gmail.com>
2025-01-22 01:43:28 +0300

    2 commit

--- src/utils/socket/socket.ts
+++ src/utils/socket/socket.ts
@@ -2,12 +2,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io();
-
-"use client";
-
-import { io } from "socket.io-client";
-
 export const socket = io({
   host: process.env.SOCKET_SERVER_HOST || "localhost",
   port: (process.env.SOCKET_SERVER_PORT as unknown as number) || 3000,
